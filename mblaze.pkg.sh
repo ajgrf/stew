00d6826be53087f6e1aaab53c826e4b0d536f844
Alex Griffin <a@ajgrf.com>
2017-10-06 19:26:52 -0500

    Add mblaze

---
+++ mblaze.pkg.sh
@@ -0,0 +1,4 @@
+name=mblaze
+version=0.2
+source=("https://github.com/chneukirchen/mblaze/releases/download/v$version/mblaze-$version.tar.gz")
+cksum=(6611387eedb9d4b18df1ae761e36292b105f8bb58d561c71ebbf88158e8d0999)
