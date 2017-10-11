8398f7fcf5577d4e5f192b042047d75b0efc90d4
Alex Griffin <a@ajgrf.com>
2017-10-11 07:55:22 -0500

    Replace paper-icon-theme with papirus-icon-theme

---
+++ papirus-icon-theme.pkg.sh
@@ -0,0 +1,4 @@
+name=papirus-icon-theme
+version=20171007
+source=("https://github.com/PapirusDevelopmentTeam/papirus-icon-theme/archive/$version.tar.gz")
+cksum=(15e031cdf940de852b75666ddbcc112d7e0ec0da090f784b6459bd5db0045509)
