d8d7d40417d5ce1aa0cd00fe361f7e99e416e555
Alex Griffin <a@ajgrf.com>
2017-11-17 09:55:36 -0600

    Add firefox

---
+++ firefox.pkg.sh
@@ -0,0 +1,12 @@
+name=firefox
+version=57.0
+source=("https://download-installer.cdn.mozilla.net/pub/firefox/releases/57.0/linux-x86_64/en-US/firefox-$version.tar.bz2" firefox.desktop)
+cksum=(c2cae016089e816c03283a359c582efab3bca34e6048ecc2382b43c1eb342457
+	5bc512b747706793365da8ed51c9550804a7ee65e9073db84ac641149756c524)
+
+install_phase() {
+	mkdir -p "$pkgdir/lib/firefox" "$pkgdir/share/applications" "$pkgdir/bin"
+	cp -r -t "$pkgdir/lib/firefox" *
+	cp ../firefox.desktop "$pkgdir/share/applications/"
+	ln -s ../lib/firefox/firefox "$pkgdir/bin/firefox"
+}
