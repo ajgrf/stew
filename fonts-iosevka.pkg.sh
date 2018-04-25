95bfc5492277617cf59cd1a66189111cb8d57cbf
Alex Griffin <a@ajgrf.com>
2018-04-25 15:47:15 -0500

    Update fonts-iosevka to 1.14.1

--- fonts-iosevka.pkg.sh
+++ fonts-iosevka.pkg.sh
@@ -1,4 +1,4 @@
 name=fonts-iosevka
-version=1.13.3
+version=1.14.1
 source=("https://github.com/be5invis/Iosevka/releases/download/v$version/iosevka-pack-$version.zip")
-cksum=(a5f29f243b9cf9dca3b252ec143d3977acb2334d832c98a55a9f07b52cb25934)
+cksum=(41d35e3ac2fce0cfc9b549e26d5d7a8246d264388cf5ae3038651a306d2d2280)
