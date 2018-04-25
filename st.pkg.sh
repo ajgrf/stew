7bb4ac5791b2865eef5bd08b6c7ec4b45813c902
Alex Griffin <a@ajgrf.com>
2018-04-25 15:20:24 -0500

    Add st desktop file

--- st.pkg.sh
+++ st.pkg.sh
@@ -2,16 +2,18 @@ name=st
 version=0.8.1
 source=("http://dl.suckless.org/st/st-$version.tar.gz" \
 	st.config.h \
+	st.desktop \
 	"https://st.suckless.org/patches/solarized/st-no_bold_colors-0.8.1.diff" \
 	"https://st.suckless.org/patches/scrollback/st-scrollback-0.8.diff")
 cksum=(c4fb0fe2b8d2d3bd5e72763e80a8ae05b7d44dbac8f8e3bb18ef0161c7266926 \
 	ef2129682b192ed3fa46d7e5a2d4791b6d197dcc2d3d7b6000f6c0f2f36ed63d \
+	515fa9786825e2369093e8c8978ffeffc2a2a4763e83ae140084fe28c53946c5 \
 	5a6e2b745c7746228e0ee4e84214e3ac7054e6d451bc5843364e878bb2011e3b \
 	8279d347c70bc9b36f450ba15e1fd9ff62eedf49ce9258c35d7f1cfe38cca226)
 
 build_phase() {
-	patch -p1 < "$builddir/${source[2]##*/}"
 	patch -p1 < "$builddir/${source[3]##*/}"
+	patch -p1 < "$builddir/${source[4]##*/}"
 	make
 }
 
@@ -19,4 +21,7 @@ install_phase() {
 	export TERMINFO="$pkgdir/share/terminfo"
 	mkdir -p "$TERMINFO"
 	autosudo make PREFIX="$pkgdir" install
+
+	mkdir -p "$pkgdir/share/applications"
+	cp ../st.desktop "$pkgdir/share/applications"
 }
