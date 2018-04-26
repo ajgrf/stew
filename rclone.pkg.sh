320eaf97d6c7b9a4d9fe8f2f21b087bb00a79e3f
Alex Griffin <a@ajgrf.com>
2018-04-25 19:25:06 -0500

    Add rclone

---
+++ rclone.pkg.sh
@@ -0,0 +1,21 @@
+name=rclone
+version=1.40
+
+unpack_phase() {
+	mkdir -p src/github.com/ncw
+	cd src/github.com/ncw
+	git clone https://github.com/ncw/rclone
+	cd ./rclone
+	git checkout "v$version"
+}
+
+build_phase() {
+	export GOPATH="$builddir"
+	go build
+}
+
+install_phase() {
+	mkdir -p "$pkgdir/bin" "$pkgdir/share/man/man1"
+	cp rclone "$pkgdir/bin"
+	cp rclone.1 "$pkgdir/share/man/man1"
+}
