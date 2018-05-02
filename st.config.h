37af2956a4a11e98018a2ec208ab6e1464576559
Alex Griffin <a@ajgrf.com>
2018-05-02 10:15:50 -0500

    st: Fix default font issue

--- st.config.h
+++ st.config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Go Mono:size=11:antialias=true:autohint=true";
+static char *font = "Go Mono:style=Regular:size=11:antialias=true:autohint=false";
 static int borderpx = 2;
 #define histsize 10000
 
--- st.pkg.sh
+++ st.pkg.sh
@@ -6,7 +6,7 @@ source=("http://dl.suckless.org/st/st-$version.tar.gz" \
 	"https://st.suckless.org/patches/solarized/st-no_bold_colors-0.8.1.diff" \
 	"https://st.suckless.org/patches/scrollback/st-scrollback-0.8.diff")
 cksum=(c4fb0fe2b8d2d3bd5e72763e80a8ae05b7d44dbac8f8e3bb18ef0161c7266926 \
-	ef2129682b192ed3fa46d7e5a2d4791b6d197dcc2d3d7b6000f6c0f2f36ed63d \
+	7abc3b6985516299f0e132c7a102c4ca42d37e508621ca55b3d591966b6a8c0d \
 	515fa9786825e2369093e8c8978ffeffc2a2a4763e83ae140084fe28c53946c5 \
 	5a6e2b745c7746228e0ee4e84214e3ac7054e6d451bc5843364e878bb2011e3b \
 	8279d347c70bc9b36f450ba15e1fd9ff62eedf49ce9258c35d7f1cfe38cca226)
