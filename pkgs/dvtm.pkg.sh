83b828f3d9dbe62c9b9a1bdea80b4feb76ace89a
Alex Griffin <a@ajgrf.com>
2018-05-09 11:22:05 -0500

    dvtm: export TERMINFO

--- pkgs/dvtm.pkg.sh
+++ pkgs/dvtm.pkg.sh
@@ -6,7 +6,7 @@ source=(dvtm.config.h)
 sum=(86e185e30b9451f6cb948bb002963973bed88e9ae0a9a4b52ba9d2a53afd3711)
 
 install_phase() {
-	TERMINFO="$pkgdir/share/terminfo"
+	export TERMINFO="$pkgdir/share/terminfo"
 	mkdir -p "$TERMINFO"
 	autosudo make PREFIX="$pkgdir" install
 }
