998e1ab0be0537e2c12566e830d4d9a18a6cb88c
Alex Griffin <a@ajgrf.com>
2018-05-09 09:42:35 -0500

    firefox: Update to 60.0

--- pkgs/firefox.pkg.sh
+++ pkgs/firefox.pkg.sh
@@ -1,7 +1,7 @@
 name=firefox
-version=57.0
-source=("https://download-installer.cdn.mozilla.net/pub/firefox/releases/57.0/linux-x86_64/en-US/firefox-$version.tar.bz2" firefox.desktop)
-sum=(c2cae016089e816c03283a359c582efab3bca34e6048ecc2382b43c1eb342457
+version=60.0
+source=("https://download-installer.cdn.mozilla.net/pub/firefox/releases/$version/linux-x86_64/en-US/firefox-$version.tar.bz2" firefox.desktop)
+sum=(185808361d70600150f3f63047fa214d86218df0ba6f3a3611f8fb23cd28b8b1
 	5bc512b747706793365da8ed51c9550804a7ee65e9073db84ac641149756c524)
 
 install_phase() {
