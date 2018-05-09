7e23696f8685d7b397378c7576e1afd975d8300e
Alex Griffin <a@ajgrf.com>
2018-05-08 19:56:38 -0500

    stack: Update to 1.7.1

--- pkgs/stack.pkg.sh
+++ pkgs/stack.pkg.sh
@@ -1,7 +1,7 @@
 name=stack
-version=1.5.1
-source=("https://github.com/commercialhaskell/stack/releases/download/v$version/stack-$version-linux-x86_64-static.tar.gz")
-cksum=(6a250cbab60870f6b933c780c96c5cafb02d066a6f2d7182bb00ec005262842b)
+version=1.7.1
+source=("https://github.com/commercialhaskell/stack/releases/download/v$version/stack-$version-linux-x86_64.tar.gz")
+cksum=(647306bbf65dd0b555486e2a32776ba1cfe0c8f380e31c6aa93abd452cb209ee)
 
 install_phase() {
 	mkdir -p "$pkgdir/bin"
