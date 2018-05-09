90001251c287cfcb942ddc8eed78c8f6bf365230
Alex Griffin <a@ajgrf.com>
2018-05-08 19:44:25 -0500

    rust: Update to 1.25.0

--- pkgs/rust.pkg.sh
+++ pkgs/rust.pkg.sh
@@ -1,7 +1,7 @@
 name=rust
-version=1.20.0
+version=1.25.0
 source=("https://static.rust-lang.org/dist/rust-$version-x86_64-unknown-linux-gnu.tar.gz")
-cksum=(ca1cf3aed73ff03d065a7d3e57ecca92228d35dc36d9274a6597441319f18eb8)
+cksum=(06fb45fb871330a2d1b32a27badfe9085847fe824c189ddc5204acbe27664f5e)
 
 install_phase() {
 	./install.sh --prefix="$pkgdir"
