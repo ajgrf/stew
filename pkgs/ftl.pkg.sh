5846df7e2a536781513977958ae845e510d436b7
Alex Griffin <a@ajgrf.com>
2018-05-10 13:47:39 -0500

    Add use function for loading support code

--- pkgs/ftl.pkg.sh
+++ pkgs/ftl.pkg.sh
@@ -5,4 +5,4 @@ buildno=18662
 source=("gogdownloader://faster_than_light/en3installer0>ftl_advanced_edition_en_${version//./_}_${buildno}.sh")
 sum=(e20ea663c666a721f31304661e43c53609d1fef3aacbc4736a385dc3eaa4d06e)
 
-. "$pkgpath/../gogenv.sh"
+use gog
--- pkgs/tis100.pkg.sh
+++ pkgs/tis100.pkg.sh
@@ -5,4 +5,4 @@ buildno=16765
 source=("gogdownloader://tis100/en3installer0>tis_100_en_${version//./_}_${buildno}.sh")
 sum=(43a25edb3136d35304324969e5616a2dda9f454994525a3fbfd5f042304d6c99)
 
-. "$pkgpath/../gogenv.sh"
+use gog
