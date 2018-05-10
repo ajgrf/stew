c305164579e627e4bfdc68d9868e6641332d60c6
Alex Griffin <a@ajgrf.com>
2018-05-10 17:37:14 -0500

    skalibs: Use preconfigure_phase

--- pkgs/skalibs.pkg.sh
+++ pkgs/skalibs.pkg.sh
@@ -2,6 +2,9 @@ name=skalibs
 version=2.6.4.0
 source=("https://github.com/skarnet/skalibs/archive/v$version.tar.gz>$name-$version.tar.gz")
 sum=(842fac5df898434e8fb15ac03e0b97745eac689339bae1af62b8906d81f16107)
-pkgdir="$stowdir/$name-$version"
-configureflags=("--prefix=$pkgdir" "--libdir=$pkgdir/lib" \
-	"--includedir=$pkgdir/include" "--sysdepdir=$pkgdir/lib/skalibs/sysdeps")
+
+preconfigure_phase() {
+	configureflags=("--prefix=$pkgdir" "--libdir=$pkgdir/lib"
+		"--includedir=$pkgdir/include"
+		"--sysdepdir=$pkgdir/lib/skalibs/sysdeps")
+}
