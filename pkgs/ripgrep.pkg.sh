854a9f33f01152931ae7c88cfcc48511d40f1052
Alex Griffin <a@ajgrf.com>
2018-05-11 13:43:29 -0500

    Move my packages to a separate repository

--- pkgs/ripgrep.pkg.sh
+++
@@ -1,7 +0,0 @@
-name=ripgrep
-version=0.8.1
-depends=(rust)
-
-install_phase() {
-	cargo install --root "$pkgdir" --vers "$version" ripgrep
-}
--- pkgs/trash-cli.pkg.sh
+++
@@ -1,6 +0,0 @@
-name=trash-cli
-version=0.17.1.14
-
-install_phase() {
-	pip3 install --system --prefix "$pkgdir" trash-cli=="$version"
-}
