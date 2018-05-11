86e30b03aa6d108745a8a13f89e6870fbb41c0f0
Alex Griffin <a@ajgrf.com>
2018-05-11 12:46:24 -0500

    Add initial documentation

--- stew.pkg.sh
+++ stew.pkg.sh
@@ -3,7 +3,8 @@ setup_phase() :
 unpack_phase() :
 
 install_phase() {
-	mkdir -p "$pkgdir/bin" "$pkgdir/lib/stew"
+	mkdir -p "$pkgdir/bin" "$pkgdir/lib/stew" "$mandir/man1"
 	cp stew "$pkgdir/bin"
 	cp stdenv.sh gog.sh "$pkgdir/lib/stew"
+	cp stew.1 "$mandir/man1"
 }
