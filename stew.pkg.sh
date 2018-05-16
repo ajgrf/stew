4251102bb16a2d6f0f183a990da95e4e269151de
Alex Griffin <a@ajgrf.com>
2018-05-16 09:58:57 -0500

    Move stew's dependencies out of stdenv

--- stew.pkg.sh
+++ stew.pkg.sh
@@ -1,5 +1,6 @@
 name=stew
 version=$(git show --no-patch --date=format:%Y%m%d --format=%cd.%h HEAD)
+aptdepends=(build-essential curl git stow)
 homepage="https://github.com/ajgrf/stew"
 description="simple package manager using GNU Stow"
 
