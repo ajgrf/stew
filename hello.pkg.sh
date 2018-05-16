3da954ff58c1ad367aa645f228514e58bd8f9768
Alex Griffin <a@ajgrf.com>
2018-05-16 12:12:21 -0500

    Stringify the remaining arrays

--- hello.pkg.sh
+++ hello.pkg.sh
@@ -2,5 +2,5 @@ name=hello
 version=2.10
 homepage="https://www.gnu.org/software/hello/"
 description="example package using GNU Hello"
-source=("https://ftp.gnu.org/gnu/hello/hello-$version.tar.gz")
-sum=(31e066137a962676e89f69d1b65382de95a7ef7d914b8cb956f41ea72e0f516b)
+source="https://ftp.gnu.org/gnu/hello/hello-$version.tar.gz"
+sum=31e066137a962676e89f69d1b65382de95a7ef7d914b8cb956f41ea72e0f516b
